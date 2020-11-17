01bc2cb0a52720d78901a6f0d1592134de2e5115
b0czek <dariusz.majnert@gmail.com>
2020-11-17 14:01:24 +0100

    split the public files into seperate css and js

---
+++ public/index.js
@@ -0,0 +1,43 @@
+const temp = document.getElementById('temperature');
+const hum = document.getElementById('humidity');
+
+const livebars = document.getElementsByClassName('live');
+changeLivebarColor = (color) => {
+    [].forEach.call(livebars, (element) => {
+        element.style.backgroundColor = color;
+    });
+};
+
+
+var socket = io();
+socket.on('data', data => {
+    temp.innerHTML = data.temp;
+    hum.innerHTML = data.hum;
+    
+    if(data.temp ===null || data.hum === null ) {
+        changeLivebarColor("red");
+    }
+    else {
+        changeLivebarColor("green");
+    }
+});
+
+socket.on('error', _ => {
+    temp.innerHTML = null;
+    hum.innerHTML = null;
+    changeLivebarColor("red");
+});
+
+//idk if it even does anything xd
+socket.on('reconnecting', (tries) => {
+    changeLivebarColor("orange");
+    if (tries === 3) {
+        changeLivebarColor("red");
+
+    }
+});
+
+socket.io.on('reconnect_error', (err) => {
+    changeLivebarColor("red");
+});
+//todo tidy up this bullshit
