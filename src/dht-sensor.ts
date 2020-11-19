b37e936403cd3902c46990df06d4f69b2b0717d3
b0czek <dariusz.majnert@gmail.com>
2020-11-19 13:34:44 +0100

    split up data change threshold to humidity and temperature

--- src/dht-sensor.ts
+++ src/dht-sensor.ts
@@ -5,14 +5,26 @@ interface SensorData {
     temp: string,
     hum: string
 }
+/**
+ * @typedef Properties
+ * @property {number} humDCT               - Threshold for humidity data to be considered changed
+ * @property {number} tempDCT              - Threshold for temperature data to be considered changed
+ * @property {number} pollingRate          - Milliseconds between read operations. Reading from sensor itself takes about 2000ms so it should be considered as pollingRate+2000  
+ */
+interface Properties {
+    humDCT?: number,
+    tempDCT?: number,
+    pollingRate?: number
+}
 
 class DHTSensor {
 
     private pin: number;
     private type: 11 | 22;
-    private pollingRate: number;
 
-    private dct: number;
+    private pollingRate = 2000;
+    private humDCT = 0.2;
+    private tempDCT = 0.2;
 
     //public event emitter destined to notify about data changing and errors
     public emitter = new EventEmitter();
@@ -20,19 +32,22 @@ class DHTSensor {
     // data contained by the object
     private temp = 0.0;
     private hum = 0.0;
+
     /**
-     * 
      * @param type type of the DHT sensor (for am2302 it is 22)
      * @param pin pin that is physically wired to the data pin of sensor
-     * @param dataChangeThreshold threshold that within which the data should not be considered as changing value, its job is to keep the database optimized by not storing as many logs
-     * @param pollingRate Milliseconds between read operations. Reading from sensor itself takes about 2000ms so it should be considered as pollingRate+2000  
+     * @param {Properties} props Additional properties for sensor
      */
-    constructor(type: 11 | 22, pin: number, dataChangeThreshold: number = 0.2, pollingRate: number = 2000) {
+    constructor(type: 11 | 22, pin: number, props?: Properties) {
         // object argument assignments
         this.pin = pin;
         this.type = type;
-        this.pollingRate = pollingRate;
-        this.dct = dataChangeThreshold;
+        // if values not provided, leave their values as initialized by default
+        if(props !== undefined) {
+            this.pollingRate = props.pollingRate != undefined ? props.pollingRate : this.pollingRate;
+            this.humDCT = props.humDCT != undefined ? props.humDCT : this.humDCT;
+            this.tempDCT = props.tempDCT != undefined ? props.tempDCT : this.tempDCT;
+        }
 
         this.readSensor();
 
@@ -46,8 +61,8 @@ class DHTSensor {
                 return;
             }
             // if any of the values fall out of threshold in any direction, then consider the value changed
-            if((temperature <= this.temp - this.dct || temperature >= this.temp + this.dct) ||
-                (humidity <= this.hum - this.dct || humidity >= this.hum + this.dct)) {
+            if((temperature <= this.temp - this.tempDCT || temperature >= this.temp + this.tempDCT) ||
+                (humidity <= this.hum - this.humDCT || humidity >= this.hum + this.humDCT)) {
                     
                     // round values to 1 decimal place - fix it to **.* notation
                     this.temp = Math.round(temperature * 100) / 100;
