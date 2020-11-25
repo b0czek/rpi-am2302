61e8482b77158373fca653d956435ace70f1e4f9
b0czek <dariusz.majnert@gmail.com>
2020-11-25 10:11:13 +0100

    zydokomunistyczny branch

--- public/index.js
+++ public/index.js
@@ -35,3 +35,75 @@ socket.io.on('reconnect_error', _ => {
     changeLivebarColor("red");
 });
 //todo tidy up this bullshit
+
+ 
+window.onload = () => {
+    //attach click listeners on all chartcontrols radio buttons
+    const chartRadios = document.getElementsByName("chartControls");
+    [].forEach.call(chartRadios, radio => {
+        radio.addEventListener("click", updateChart);
+    });
+    
+};
+
+let awaitingResponse = false;
+
+const updateChart = () => {
+    let selected = document.querySelector('input[name="chartControls"]:checked').value;
+    switch(selected) {
+        case "allTime":
+            
+    }
+    awaitingResponse = true;
+};
+
+// Date.prototype.stdTimezoneOffset = function () {
+//     var jan = new Date(this.getFullYear(), 0, 1);
+//     var jul = new Date(this.getFullYear(), 6, 1);
+//     return Math.max(jan.getTimezoneOffset(), jul.getTimezoneOffset());
+// }
+
+// Date.prototype.isDstObserved = function () {
+//     return this.getTimezoneOffset() < this.stdTimezoneOffset();
+// }
+
+
+
+const ctx = document.getElementById('chartCtx').getContext('2d');
+let chart = new Chart(ctx, {
+    type: 'line',
+    data: {
+        datasets: [{
+            label: 'Temperatura',
+            borderColor: 'rgba(255,69,0,1)',
+            borderWidth: 3,
+            fill: false
+        }, {
+            label: 'Wilgotność',
+            borderColor: 'rgba(75, 192, 192, 1)',
+            borderWidth: 3,
+            fill:false
+        }]
+    },
+    options: {
+        scales: {
+            xAxes: [{
+                type: 'time',
+                distribution: 'linear',
+                time: {
+                    isoWeekday: true,
+                    displayFormats: {
+                        hour: 'HH'
+                        
+                    }
+                }
+            }],
+            yAxes: [{
+                ticks: {
+                    beginAtZero: true,
+                    suggestedMax: 100
+                }
+            }]
+        }
+    }
+});
--- src/index.ts
+++ src/index.ts
@@ -13,10 +13,10 @@ const EXPRESS_PORT = 4000;
 
 
 const main = async () => {
-    
+
     const orm = await MikroORM.init(mikroConfig);
     await orm.getMigrator().up();
-    
+
     //init sensors
     let sensor = new Sensor(TYPE, PIN, { humDCT: 0.3 });
 
@@ -31,16 +31,53 @@ const main = async () => {
     app.use('/', express.static('public'));
     const server = app.listen(EXPRESS_PORT, () => {
         console.log(`app started on port ${EXPRESS_PORT}`);
-    
+
     });
 
     //socketio initialization
     const io = new Server(server);
-    
+
     io.on('connect', (socket: Socket) => {
-        //send data to connecting client so it wont have to wait for the next datachange
+        //send data to connecting client so they wont have to wait for the next datachange
         socket.emit('data', sensor.getData());
+        // socket endpoint for requesting data used in charts, parameter dataFrom is required, dataTo is concluded to be current date by default
+        socket.on('chartDataRequest', async (dataFrom: any, dataTo = (new Date()).toISOString()) => {
+            console.log('got data request');
+            // check if datafrom is actually provided 
+            if(dataFrom == undefined) {
+                socket.emit('chartDataResponse', {
+                    error: true
+                });
+                return;
+            }
+
+            // create new date objects from parameters
+            dataFrom = new Date(dataFrom).toISOString();
+            dataTo = new Date(dataTo).toISOString();
+
+            // if any of them is invalid, then return
+            if(dataFrom == "Invalid Date" || dataTo == "Invalid Date") {
+                socket.emit('chartDataResponse', {
+                    error: true
+                });
+                return;
+            }
+
+            // fetch data from db
+            let logs: Log[] = await orm.em.find(Log, { date: {
+                $gte: dataFrom,
+                $lt: dataTo
+            } });
+            
+            console.log(`datafrom: ${dataFrom}, datato ${dataTo}`);
+            console.log(logs);
+            socket.emit("chartDataResponse", {
+                error: false, 
+                data: logs
+            });
+        });
     });
+
 
     sensor.emitter.on('dataChanged', async (temperature, humidity) => {
         io.emit('data', Sensor.createDataObject(temperature, humidity));
@@ -48,15 +85,15 @@ const main = async () => {
         const log = orm.em.create(Log, { humidity: humidity, temperature: temperature });
         await orm.em.persistAndFlush(log);
     });
-    
+
     sensor.emitter.on('dataError', () => {
         io.emit('error');
     });
-    
+
 };
 
 
 main().catch(err => {
     console.error(err);
-    
+
 });
